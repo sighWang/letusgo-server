d664f59a374ca6e2ad193720016b4eb2bd240a16
sigh <wangximingde@163.com>
2014-09-18 01:00:03 +0800

    try to response client

--- app.js
+++ app.js
@@ -56,5 +56,17 @@ if (app.get('env') === 'production') {
     });
   });
 }
-
+var client =  redis.createClient();
+var cocacola = {id: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, category: 'drink'};
+var sprite = {id: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, category: 'drink'};
+var lychee = {id: 'ITEM000002', name: '荔枝', unit: '斤', price: 15.00, category: 'fruit'};
+var badminton = {id: 'ITEM000003', name: '羽毛球', unit: '个', price: 4.50, category: 'sport'};
+var goodsList = [];
+goodsList.push(cocacola, sprite, lychee, badminton);
+client.set('goodsList', goodsList);
+client.set('catagary', [
+  {id: 1, name: 'drink'},
+  {id: 2, name: 'fruit'},
+  {id: 3, name: 'sport'}
+]);
 module.exports = app;
--- router/routes/items.js
+++ router/routes/items.js
@@ -4,7 +4,16 @@ var redis = require('redis');
 var client =  redis.createClient();
 router.get('/', function(req, res) {
   //TODO: Need to implement.
-  res.send('Success!');
+  res.send('success');
+//  var items = client.get('goodsList');
+//  console.log(items);
+//  res.send(items);
+});
+
+router.get('/api/items', function (req, res) {
+  var items = client.get('goodsList');
+  console.log(items);
+  res.send(items);
 });
 
 module.exports = router;

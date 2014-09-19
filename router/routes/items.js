c04325fa37e54840122f48234fa4169b347bbb95
sigh <wangximingde@163.com>
2014-09-19 11:49:44 +0800

    fix items

--- router/routes/items.js
+++ router/routes/items.js
@@ -4,10 +4,20 @@ var redis = require('redis');
 var client =  redis.createClient();
 router.get('/', function(req, res) {
   //TODO: Need to implement.
-  res.send('success');
-//  var items = client.get('goodsList');
-//  console.log(items);
-//  res.send(items);
+////  res.send('helo my dear');
+//  client.lrange('goodsList', 0, -1, function (err, goodsList) {
+//    res.send(JSON.parse(goodsList));
+//  });
+  client.get('items', function (err, obj) {
+    res.send(JSON.parse(obj));
+  });
+//  client.get('massage',function (err, goodsList) {
+//    res.send(goodsList);
+//  });
+//  client.lindex('goodsList',0, function (err, item) {
+//    res.send(item);
+//  });
+
 });
 
 router.get('/api/items', function (req, res) {

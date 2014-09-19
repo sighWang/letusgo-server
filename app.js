a87b65e1d2e6f7c5f597fd3874a831c40815f46a
sigh <wangximingde@163.com>
2014-09-19 21:25:08 +0800

    delete useless codes

--- app.js
+++ app.js
@@ -63,13 +63,6 @@ var lychee = {id: 'ITEM000002', name: '荔枝', unit: '斤', price: 15.00, categ
 var badminton = {id: 'ITEM000003', name: '羽毛球', unit: '个', price: 4.50, category: 'sport'};
 var goodsList = [];
 goodsList.push(cocacola, sprite, lychee, badminton);
-client.rpush('goodsList', JSON.stringify(cocacola), function (err, relpy) {});
-client.set('massage','hello');
-//client.lpush('catagary',
-//  {id: 1, name: 'drink'},
-//  {id: 2, name: 'fruit'},
-//  {id: 3, name: 'sport'}
-//);
 var categories = [ {id: 1, name: 'drink'},
   {id: 2, name: 'fruit'},
   {id: 3, name: 'sport'}];
--- router/routes/categories.js
+++ router/routes/categories.js
@@ -4,7 +4,6 @@ var redis = require('redis');
 var client =  redis.createClient();
 
 router.get('/manage', function(req, res) {
-  //TODO: Need to implement.
   client.get('categories', function (err, obj) {
     res.send(JSON.parse(obj));
   });

b350e703c1cd88584e37aaf89bc40127c85067b0
sigh <wangximingde@163.com>
2014-09-20 20:59:33 +0800

    add delete category code

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -2,16 +2,24 @@ var express = require('express');
 var router = express.Router();
 var redis = require('redis');
 var client = redis.createClient();
-
+client.hset('test','1','sigh');
+router.get('/test', function (req, res){
+  console.log('enter');
+  res.send(client.hget('test','1'));
+});
 router.get('/manage', function (req, res) {
   client.get('categories', function (err, obj) {
     res.send(JSON.parse(obj));
   });
 });
 
-router.post('/delete', function (req, res) {
-  res.send('enter delete' + req.param('data'));
+router.post('/', function (req, res) {
+ // client.set('categories', req.body);
   console.log(req.param('id'));
+  res.send(req.param('id'));
 });
 
+router.delete('/',function (req, res) {
+  res.send(req.param('id'));
+});
 module.exports = router;

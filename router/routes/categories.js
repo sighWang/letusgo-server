256842b5121f13eb099c3ada3256235651d51661
sigh <wangximingde@163.com>
2014-09-20 21:24:24 +0800

    delete useless code

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -4,20 +4,13 @@ var redis = require('redis');
 var client = redis.createClient();
 client.HMSET('categories', '1','drink', '2','fruit', '3','sport');
 
-client.hset('test', '1', 'sigh');
-router.get('/test', function (req, res) {
-  console.log('enter');
-  res.send(client.hget('test', '1'));
-});
-router.get('/manage', function (req, res) {
-  client.get('categories', function (err, obj) {
+router.get('/', function (req, res) {
+  client.hgetall('categories', function (err, obj) {
     res.send(JSON.parse(obj));
   });
 });
 
 router.post('/', function (req, res) {
-  // client.set('categories', req.body);
-  console.log(req.param('id'));
   res.send(req.param('id'));
 });
 

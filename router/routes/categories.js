97052d1ddd2ef433008264787d9ad0be3d22293f
sigh <wangximingde@163.com>
2014-09-21 20:39:52 +0800

    change object to array

--- router/routes/categories.js
+++ router/routes/categories.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('redis');
+var _ = require('lodash');
 var client = redis.createClient();
 client.hmset('category', '1', "drink", '2', "fruit", '3', "sport");
 router.get('/', function (req, res) {
   client.hgetall('category', function (err, obj) {
-    res.send(obj);
+    var categories = [];
+    _.forEach(obj, function (name,index){
+        categories.push({id:index, name:name});
+    });
+    res.send(categories);
   });
 });
 

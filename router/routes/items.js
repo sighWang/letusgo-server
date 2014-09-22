ea751d29dbf7e7abe4ab243687a31fedf6fc4e27
sigh <wangximingde@163.com>
2014-09-22 11:09:52 +0800

    change items storage format

--- router/routes/items.js
+++ router/routes/items.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('redis');
+var _ = require('lodash');
 var client = redis.createClient();
-var cocacola = {id: 'ITEM000000', name: '可口可乐', unit: '瓶', price: 3.00, category: 'drink'};
-var sprite = {id: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, category: 'drink'};
-var lychee = {id: 'ITEM000002', name: '荔枝', unit: '斤', price: 15.00, category: 'fruit'};
-var badminton = {id: 'ITEM000003', name: '羽毛球', unit: '个', price: 4.50, category: 'sport'};
-var goodsList = [];
-goodsList.push(cocacola, sprite, lychee, badminton);
-client.set('items', JSON.stringify(goodsList), function (err, reply) {
-});
+var cocacola = {name: '可口可乐', unit: '瓶', price: 3.00, category: 'drink'};
+var sprite = {name: '雪碧', unit: '瓶', price: 3.00, category: 'drink'};
+var lychee = {name: '荔枝', unit: '斤', price: 15.00, category: 'fruit'};
+var badminton = {name: '羽毛球', unit: '个', price: 4.50, category: 'sport'};
+client.hmset('itemList', 'ITEM000000', JSON.stringify(cocacola),
+  'ITEM000001', JSON.stringify(sprite),
+  'ITEM000002', JSON.stringify(lychee),
+  'ITEM000003', JSON.stringify(badminton));
+
 client.set('customItems', JSON.stringify([]));
 
 router.get('/', function (req, res) {
-  client.get('items', function (err, obj) {
-    res.send(JSON.parse(obj));
+  client.hgetall('itemList', function (err, obj) {
+    var items = [];
+    _.forEach(obj, function (data, index) {
+      var item = JSON.parse(data);
+      items.push({id:index, name:item.name, unit:item.unit, price:item.price, category:item.category });
+    });
+    res.send(items);
   });
 });
 

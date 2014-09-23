6cdbe39fca0f04fd8a06aa0574f641a90a2582c5
sigh <wangximingde@163.com>
2014-09-23 09:36:37 +0800

    optimize code

--- router/routes/customItems.js
+++ router/routes/customItems.js
@@ -5,39 +5,40 @@ var _ = require('lodash');
 var client = redis.createClient();
 
 
-function countLength(obj){
+function countLength(obj) {
   var j = 1;
-  _.forEach(obj, function (number, index) {
+  _.forEach(obj, function () {
     j++;
   });
   return j;
 }
 
 router.get('/', function (req, res) {
-  var i = 1, customItems = [];
+  var i = 1;
   var total = 0;
+  var customItems = [];
   client.hgetall('customItemsWang', function (err, obj) {
     var length = countLength(obj);
     _.forEach(obj, function (number, index) {
-      client.hget('itemList', index, function (err, data){
+      client.hget('itemList', index, function (err, data) {
         var item = JSON.parse(data);
         var subtotal = item.price * number;
         var goods = {id: index, name: item.name, unit: item.unit, price: item.price, category: item.category };
-        customItems.push({goods: goods, number: number ,subtotal:subtotal});
+        customItems.push({goods: goods, number: number, subtotal: subtotal});
 
-        total += item.price * number;
         i++;
-        if(i === length){
-          var categories = _.groupBy(customItems, function (custom){
+        if (i === length) {
+          var categories = _.groupBy(customItems, function (custom) {
             return custom.goods.category;
           });
+          total += item.price * number;
           res.send({categories: categories, total: total});
         }
 
       })
     })
   });
-  });
+});
 
 
 router.post('/:customItem', function (req, res) {
